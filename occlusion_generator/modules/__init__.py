78201856c01491068e5c8edeac5d25d6b3854d66
Корсаев Артемий Батаевич <abkorsaev@edu.hse.ru>
2026-08-19 14:44:43 +0700

    fix: init issue

--- occlusion_generator/modules/__init__.py
+++ occlusion_generator/modules/__init__.py
@@ -1,6 +1,6 @@
 from .fog import FogModule
 from .reflection import ReflectionModule
-from ...soiling import SoilingModule
+from ..soiling import SoilingModule
 from .flare import FlareModule
 
 __all__ = ['FogModule', 'ReflectionModule', 'SoilingModule', 'FlareModule']
